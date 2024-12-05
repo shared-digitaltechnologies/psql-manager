ebe2390fedf638b8d35b7d8ebac85f607b4d5449
roelhem <ik@roelweb.com>
2024-12-05 13:32:36 +0100

    Fix env map

--- config.go
+++ config.go
@@ -112,46 +112,6 @@ func NewConnString() ConnString {
 	}
 }
 
-var settingAlias = map[string]string{
-	"dbname": "database",
-}
-
-var envToSettingList = []struct {
-	env     string
-	setting string
-}{
-	{"DB_HOST", "host"},
-	{"POSTGRES_HOST", "host"},
-	{"PGHOST", "host"},
-	{"DB_PORT", "port"},
-	{"POSTGRES_PORT", "port"},
-	{"PGPORT", "port"},
-	{"DB_DATABASE", "database"},
-	{"POSTGRES_DATABASE", "database"},
-	{"PGDATABASE", "database"},
-	{"DB_USER", "user"},
-	{"POSTGRES_USER", "user"},
-	{"DB_USERNAME", "user"},
-	{"POSTGRES_USERNAME", "user"},
-	{"PGUSER", "user"},
-	{"DB_PASSWORD", "user"},
-	{"PGPASSWORD", "password"},
-	{"PGPASSFILE", "passfile"},
-	{"PGAPPNAME", "application_name"},
-	{"PGCONNECT_TIMEOUT", "connect_timeout"},
-	{"DB_SSL", "sslmode"},
-	{"POSTGRES_SSL", "sslmode"},
-	{"PGSSLMODE", "sslmode"},
-	{"PGSSLKEY", "sslkey"},
-	{"PGSSLCERT", "sslcert"},
-	{"PGSSLSNI", "sslsni"},
-	{"PGSSLROOTCERT", "sslrootcert"},
-	{"PGSSLPASSWORD", "sslpassword"},
-	{"PGTARGETSESSIONATTRS", "target_session_attrs"},
-	{"PGSERVICE", "service"},
-	{"PGSERVICEFILE", "servicefile"},
-}
-
 func (c *ConnString) Copy() (res ConnString) {
 	res.Type = c.Type
 	res.settings = make(map[string]string)
@@ -348,15 +308,6 @@ func (c *ConnString) LoadDefaultUserSettings() error {
 	return err
 }
 
-func (c *ConnString) LoadEnvSettings() {
-	for _, val := range envToSettingList {
-		value := os.Getenv(val.env)
-		if value != "" {
-			c.settings[val.setting] = value
-		}
-	}
-}
-
 func defaultHost() string {
 	candidatePaths := []string{
 		"/var/run/postgresql", // Debian
@@ -398,16 +349,6 @@ func (c *ConnString) String() string {
 	default:
 		return c.StringKeywordValue()
 	}
-}
-
-func (c *ConnString) Env() []string {
-	var res []string
-	for _, pair := range envToSettingList {
-		if val, present := c.settings[pair.setting]; present {
-			res = append(res, pair.env+"="+val)
-		}
-	}
-	return res
 }
 
 func (c *ConnString) LoadConnString(connString string) error {
